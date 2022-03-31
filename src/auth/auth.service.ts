8336e4517ef2561ae73e13146686dba30c9753b4
Caio R. Vieira <caio.robs98@gmail.com>
2022-03-31 18:00:30 -0300

    importin db with global prisma module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 // import { User, Bookmark } from '@prisma/client';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signUp() {
     return { message: 'i am signed up' };
   }
 
   signIn() {
-    return { message: 'i am signed up' };
+    return { message: 'i am signed in' };
   }
 }
